ba5e40d81a655617aa9720da64f16df0f106dce6
RamonC3 <130200691+RamonC3@users.noreply.github.com>
2023-04-27 17:21:40 -0700

    Update insertion.h

--- insertion.h
+++ insertion.h
@@ -1,6 +1,36 @@
-// insertion sort header file
+/*
+void insertionSort(int arr[], int n){
+     int i, key, j;
+     for (i = 1; i < n; i++){
+        key = arr[i];
+        j = i - 1;
 
-// declaration of insertion sort function with array passed by reference
-void insertionSort(int (&arr)[10], int size);
+        while (j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j--;
+        }//while
+        arr[j + 1] = key;
+    }//for
+}//insertionSort
 
+void printArray(int arr[], int n){
+     int i;
+     for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+     cout << endl;
+}//printArray
 
+//my driver code
+/*
+int main(){
+    int arr[] = { 6,2,8,3,4,58,96,12,45 };
+    int N = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before sorting... "<<endl;
+    printArray(arr,N);
+    cout << "After Sorting..." << endl;
+    insertionSort(arr, N);
+    printArray(arr, N);
+
+    return 0;
+}//main
+*/*/
